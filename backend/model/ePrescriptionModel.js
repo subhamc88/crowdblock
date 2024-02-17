5cc97d813dfe37df11b414da73105e309f0e67c6
jyotii25 <patnaikjyoti25@gmail.com>
2024-02-17 16:36:42 +0530

    E-prescription model added

---
+++ backend/model/ePrescriptionModel.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose')
+const ePrescriptionModel = mongoose.model("prescription", new mongoose.Schema({
+  userName: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  docName: {
+    type: String,
+    required: true,
+  },
+  prescriptionDate: {
+    type: String,
+    required: true,
+  },
+  medicationDetails: {
+    type: String,
+    required: [true, "name of the medicine, doses required,doses timing are to be filled"],
+
+  },
+  specialInformation: {
+    type: String,
+  },
+  time: {
+    type: String,
+    required: true,
+  },
+},
+  { timestamp: true }
+))
