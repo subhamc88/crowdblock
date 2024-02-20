fe61095dc5e93a90202c8caf978e0713b45cba9c
Coder <coder@pradeeps-MacBook-Air.local>
2024-02-20 12:08:55 +0530

    mongo compass actually added this time

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const db = async () => {
-  await mongoose.connect("mongodb://localhost:27017/")
+  await mongoose.connect("mongodb+srv://jyotii25:queuemedi@mediq.p9gdaoi.mongodb.net/?retryWrites=true&w=majority")
     .then(await mongoose.connection.once('open', () => console.log(`Database Connected`)))
   await mongoose.connection.on('error', (error) => console.log(error))
 }
